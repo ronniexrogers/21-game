60e09efc8d6a8edd2bf55462ac4fa8bce754a880
ronniexrogers <ronniexrogers@yahoo.com>
2022-01-05 07:32:54 -0800

    created a different deck with number values

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-Creating cards/numbers
-let suits = ["♠", "♣", "♥", "♦"]
-let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
-let deck = []
+//Creating cards/numbers
+// let suits = ["♠", "♣", "♥", "♦"]
+// let ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+// let deck = []
 
-for(let i=0; i<4; i++) {
-    for(let j=0; j<13; j++) {
-        deck.push(ranks[j] + suits[i])
-    }
+// for(let i=0; i<4; i++) {
+//     for(let j=0; j<13; j++) {
+//         deck.push(ranks[j] + suits[i])
+//     }
+// }
+// console.log(deck)
+const deck = [
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11, 
+    2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11
+]
+
+function drawRandomCard(deck) {
+    let randomIndex = Math.floor(deck.length * Math.random())
+    return deck[randomIndex]
 }
-console.log(deck)
+console.log(drawRandomCard(deck))
 
 
 
